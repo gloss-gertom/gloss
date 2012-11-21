f40cc3026357b3d54de8299105775589c61c8061
trmcmanus <tmcmanus@ymail.com>
2012-11-21 14:33:13 +0000

    Seed data - Categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
+#Seed data for Gloss HomeStore
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#NOTE: All data is DELETED then reloaded. New() method is used in order to be able to specify primary key values.
+#
+########################################
+Category.delete_all
+
+cat=Category.new
+cat.id = 1
+cat.category_name = "Audio"
+cat.save
+
+cat=Category.new
+cat.id = 2
+cat.category_name = "Furniture"
+cat.save
+
+cat=Category.new
+cat.id = 3
+cat.category_name = "PC"
+cat.save
+
+cat=Category.new
+cat.id = 4
+cat.category_name = "TV"
+cat.save
+
+
+
