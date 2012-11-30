47e8ec01d65137f6cb01ca03bbd61e24cadec65f
Ger Lowry <lowrygerard@gmail.com>
2012-11-30 15:00:40 +0000

    Added Login form to header area, after creating a partial, added page title to pages and included some javascript for onfocus and onblur events in login form

---
+++ app/assets/javascripts/focus.js
@@ -0,0 +1,10 @@
+function myFocus(element) {
+    if (element.value == element.defaultValue) {
+        element.value = '';
+    }
+}
+function myBlur(element) {
+      if (element.value == '') {
+          element.value = element.defaultValue;
+      }
+}
