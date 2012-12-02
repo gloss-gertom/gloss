fddb9d3f210c9cf4281c0e742e9d54d00697c9a7
trmcmanus <tmcmanus@ymail.com>
2012-12-02 18:11:32 +0000

    Amend NavBar - product list filter by category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,11 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    #NOTE: search/find by category_name is temporary.
+    #When Category navBar is amended to use Category Model then category.id will be known and passed to Products.
+    #
+    @category = Category.find_by_category_name(params[:category_name])
+
+    @products = Product.find_all_by_category_id(@category.id)
 
     respond_to do |format|
       format.html # index.html.erb
