6a1419f7b109b3852d95980d240d65e2b94a2c6d
trmcmanus <tmcmanus@ymail.com>
2012-12-18 17:07:37 +0000

    Add 'quick' add_to_cart button, total_quantity to cart summary and update totals heading in Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -21,6 +21,9 @@ class Cart < ActiveRecord::Base
     current_item
   end
 
+  def total_quantity
+    line_items.to_a.sum { |item| item.quantity}
+  end
 
   def total_price
     line_items.to_a.sum { |item| item.total_price }
