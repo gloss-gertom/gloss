174fe48b601ecc9ae951634086f50a1f7de9d8c0
Gerard Lowry <lowrygerard@gmail.com>
2013-01-10 14:48:45 +0000

    First pass at front page. Implemented the special offers and featured products. Need to re-visit the page_controller to improve the query and then update either controller or view template to include a conditional on the query

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,7 +1,14 @@
 class PageController < ApplicationController
+
  
   def home
- 
+  #TODO repeating myself here, need to rewrite
+  # getting featured and special offers as two queries
+  # need to refine down to one query
+  # and then do a conditional check and return
+  @features = Product.where(:featured_product => true).limit(3)
+  @specials = Product.where(:special_offer => true).limit(4)
+
   end
 
   def about
